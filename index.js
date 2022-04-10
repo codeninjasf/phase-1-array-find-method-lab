9b21b9908c29b4e0d341f258cff8035b150fc286
arturoalba <ajdalba@gmail.com>
2022-04-10 16:14:40 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function superbowlWin(record){
+    function isFootballYear(year){
+        if (year.result === "W"){
+            return true
+        } else {
+            return false
+        }
+    }
+
+
+const yearWon = record.find(isFootballYear);
+    if (yearWon){
+        return yearWon.year
+    }
+
+}
